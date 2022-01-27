b6637a082c66ab32ea55ab6a61496fe4334146b2
CHIHYUNGLEE <eno125@naver.com>
2022-01-27 23:36:45 +0900

    yml, test update

--- recoBOOTJPA/src/test/java/com/reco/customer/dao/CustomerDAOOracleTest.java
+++ recoBOOTJPA/src/test/java/com/reco/customer/dao/CustomerDAOOracleTest.java
@@ -3,13 +3,33 @@ package com.reco.customer.dao;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.reco.exception.FindException;
 @SpringBootTest
 class CustomerDAOOracleTest {
 
+	@Autowired
+	private CustomerDAOInterface dao;
+	
 	@Test
-	void test() {
-		
+	void testFindByEmail() {
+		String email = "";
+		try {
+			dao.findByEmail(email);
+		} catch (FindException e) {
+			e.printStackTrace();
+		}
 	}
-
+	
+	@Test
+	void testFindByNick() {
+		String nick = "";
+		try {
+			dao.findByNick(nick);
+		} catch (FindException e) {
+			e.printStackTrace();
+		}
+	}
 }
