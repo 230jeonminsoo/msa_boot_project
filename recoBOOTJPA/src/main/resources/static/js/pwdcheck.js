208b82f0418ab99677fce8aa2d8fe32a7cde1f6d
CHIHYUNGLEE <eno125@naver.com>
2022-02-10 15:48:40 +0900

    마이페이지 부분수정

--- recoBOOTJPA/src/main/resources/static/js/pwdcheck.js
+++ recoBOOTJPA/src/main/resources/static/js/pwdcheck.js
@@ -1,9 +1,10 @@
 /** 확인 버튼 클릭되었을 떄*/
 function checkBtClick(pwd){
 	console.log(pwd);
-	let $pwd = $('div.pwdcheck>input[type=password]').val();
+	
 	let $checkBtObj = $('button.pwdcheck');
 	$checkBtObj.click(function(){
+		let $pwd = $('div.pwdcheck>input[type=password]').val();
 		console.log($pwd);
 		if(pwd == $pwd){
 			console.log($pwd);
--- recoBOOTJPA/src/main/resources/static/js/tab.js
+++ recoBOOTJPA/src/main/resources/static/js/tab.js
@@ -50,7 +50,7 @@ function calMenuClick(){ //callistresult.jsp
 
 
 function tabMenuClick(){
-	let $tabMenuObj = $('div.tab>ul.communitytab>li>a');
+	let $tabMenuObj = $('div.tab>ul>li>a');
 	 $tabMenuObj.click(function(){
         let menuHref = $(this).attr('href'); 
         console.log("메뉴 href=" + menuHref);
@@ -116,13 +116,18 @@ function tabMenuClick(){
 			//tab에서 개인정보 관리를 클릭되었을때
 			case 'myprivate':
 				ajaxUrl = menuHref;
-                $('section>div.articles').empty();
-                $('section>div.articles').load(ajaxUrl,function(responseText, textStatus, jqXHR){
-                    if(jqXHR.status != 200){
-                        alert('응답실패:' + jqXHR.status);
-                    }
-                });
-                return false;
+				alert(ajaxUrl);
+	            $('section>div.articles').empty();
+	            $('section>div.articles').load(ajaxUrl,function(responseText, textStatus, jqXHR){
+	                if(jqXHR.status != 200){
+	                    alert('응답실패:' + jqXHR.status);
+	                }
+	            });
+	            return false;
+			
+			default:
+			   alert("menu-" + menuHref);
+				break;
          }
 	});
 }
