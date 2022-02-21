80d065bbe6eee76f26243c754fe837e7414a497b
invertgom <invertgom@gmail.com>
2022-02-21 20:11:17 +0900

    calpost

--- recoBOOTJPA/src/main/java/com/reco/calendar/dao/CalendarDAOOracle.java
+++ recoBOOTJPA/src/main/java/com/reco/calendar/dao/CalendarDAOOracle.java
@@ -111,13 +111,15 @@ public class CalendarDAOOracle implements CalendarDAOInterface {
 
 	
 //	@Override
-	public void modifyCal(CalInfo calinfo) throws ModifyException{
-//
+	public void modifyCal(CalInfo calinfo) throws ModifyException {
+//		SqlSession session = null;
+//			
 //		int uIdx = calinfo.getCustomer().getUIdx();
 //		int calIdx = calinfo.getCalIdx();
-////		String calCategory = calinfo.getCalCategory();
-////		String calThumbnail = calinfo.getCalThumbnail();
+//		
 //		try {
+//			session = sqlSessionFactory.openSession();
+//			
 //			List<CalInfo> list = findCalsByUIdx(uIdx);
 //			Connection con = null;
 //			PreparedStatement pstmt = null;
@@ -131,7 +133,7 @@ public class CalendarDAOOracle implements CalendarDAOInterface {
 //			pstmt.setInt(2, calinfo.getCalIdx());
 //			pstmt.executeUpdate();
 //			pstmt = con.prepareStatement(modifySQL1);
-//			pstmt.setString(1, calinfo.getCalThumbnail());
+//			pstmt.setString(캘린더 게시글이 없습니다1, calinfo.getCalThumbnail());
 //			pstmt.setInt(2, calinfo.getCalIdx());
 //			pstmt.executeUpdate();
 //			con.commit();
