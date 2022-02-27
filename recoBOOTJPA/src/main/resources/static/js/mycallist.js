dd0247ed5a3e25199fefa2a2f63b879fc225900a
invertgom <invertgom@gmail.com>
2022-02-27 17:49:53 +0900

    Update mycallist.js
    캘린더 삭제 취소 추가

--- recoBOOTJPA/src/main/resources/static/js/mycallist.js
+++ recoBOOTJPA/src/main/resources/static/js/mycallist.js
@@ -1,3 +1,46 @@
+$('button.mycalRM').click(function(){
+		//$removeBtnObj.click(function(){
+		console.log("removeBtnClick()");
+		if(confirm("해당 캘린더를 삭제하시겠습니까?")==true){ //확인
+
+		}else{ //취소 
+			return false;
+		}
+		
+		// 선택된 목록 가져오기
+		const query = 'input[name="calIdx"]:checked';
+		console.log(query);	  	
+	  	const selectedEls = 
+      		document.querySelectorAll(query);
+		
+		// 선택된 목록에서 value 찾기
+		let data = '';
+	  	selectedEls.forEach((el,index) => {
+	    data += 'calIdx'+'='+el.value;
+		});
+		
+		console.log(data);
+		let ajaxUrl = "./calendarRemove";	 
+		    
+		$.ajax({
+            url: ajaxUrl,
+            method : 'get',
+			data: data,
+            success:function(responseData){
+				let $articlesObj = $('section>div.articles');//callistresutl.jsp의 섹션
+				$articlesObj.empty();
+                $articlesObj.html(responseData);
+				//history.go(-1);
+				location.reload(); 
+
+		    }
+        }); 
+		
+        return false;
+			
+	});
+
+
 /*--mycallist.jsp화면에서 캘린더 썸네일 클릭했을때--*/
 /*function mycalThumbnailClick(option){
 	
@@ -34,46 +77,3 @@
 		});
 	
 }*/
-	
-/*-mycallist.jsp화면에서 체크박스 클릭하고 삭제버튼 클릭 했을때 */
-function removeBtnClick(){
- 	let $removeBtnObj = $('button.mycalRM');
- 		console.log("removeBtnClick()");
-	
-	$removeBtnObj.click(function(){
-		alert("해당 캘린더를 삭제하시겠습니까?");
-		self.close();
-		// 선택된 목록 가져오기
-		const query = 'input[name="calIdx"]:checked';
-		console.log(query);	  	
-	  	const selectedEls = 
-      		document.querySelectorAll(query);
-		
-		// 선택된 목록에서 value 찾기
-		let data = '';
-	  	selectedEls.forEach((el,index) => {
-	    data += 'calIdx'+'='+el.value;
-		});
-		
-		console.log(data);
-		let ajaxUrl = "./calendarRemove";	 
-		    
-		$.ajax({
-            url: ajaxUrl,
-            method : 'get',
-			data: data,
-            success:function(responseData){
-				let $articlesObj = $('section>div.articles');//callistresutl.jsp의 섹션
-				$articlesObj.empty();
-                $articlesObj.html(responseData);
-				//history.go(-1);
-				location.reload(); 
-
-		    }
-        }); 
-		
-        return false;
-			
-	});
-}
-
