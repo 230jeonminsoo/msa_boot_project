6574b8f8c2dfe7aaec2f5a678b3d68fadebf934d
CHIHYUNGLEE <eno125@naver.com>
2022-03-03 18:48:21 +0900

    Update CustomerService.java

--- recoBOOTJPA/src/main/java/com/reco/customer/service/CustomerService.java
+++ recoBOOTJPA/src/main/java/com/reco/customer/service/CustomerService.java
@@ -110,7 +110,7 @@ public class CustomerService {
 	        params.put("to", phoneNumber);    // 수신전화번호
 	        params.put("from", "01071354330");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
 	        params.put("type", "SMS");
-	        params.put("text", "RECO 휴대폰인증 테스트 메시지 : 인증번호는" + "["+numStr+"]" + "입니다.");
+	        params.put("text", "안녕하세요 RECO입니다. 인증번호는 " + "["+numStr+"]" + " 입니다.");
 	        params.put("app_version", "test app 1.2"); // application name and version
 
 	        try {
